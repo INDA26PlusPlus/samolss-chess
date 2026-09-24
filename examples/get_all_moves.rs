3202a12215d84dbbf1913dc7f438ba488c8ad0fa
Samuel Olsson <samuel.olsson@hitachigymnasiet.se>
2026-09-24 19:43:41 +0200

    Add generating moves for specific piece to example

--- examples/get_all_moves.rs
+++ examples/get_all_moves.rs
@@ -8,6 +8,12 @@ fn main() {
     // and a char denoting what the promotion piece should be.
     println!("{:?}", move_piece::gen_all_moves(&board));
 
+    println!("");
+
+    // This will generate all LEGAL moves for a specific piece on the board (denoted by the square
+    // its on). In this case the piece is the pawn on e2
+    println!("{:?}", move_piece::gen_moves_for_piece(&board, (4, 1)));
+
     // Good to know is that when castling the old square should be the kings square and the new
     // square should be the rooks square.
 }
